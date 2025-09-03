f798d3aa44bdca811636ca3f8cd63ba2aa21998c
Bhumika Singh Rawat <bhumika282007@gmail.com>
2025-09-03 17:31:42 +0530

    GREAYER NUMBER
    Find the largest of two numbers.

---
+++ GREATER_.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+int a,b;
+
+printf("ENTER ANY TWO NUMBERS");
+scanf("%d",&a,&b);
+
+if(a>b)
+{
+printf("%d IS THE LARGEST NUMBER");
+}
+else
+{
+printf("%d IS THE LARGEST NUMBER");
+}
+
+getch();
+
+}
