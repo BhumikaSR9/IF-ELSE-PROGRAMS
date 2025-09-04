2fdc0621b133576a07f6f0d7d56b5f22d527472c
Bhumika Singh Rawat <bhumika282007@gmail.com>
2025-09-04 16:58:50 +0530

    LEAP YEAR
    Check whether a year is leap year or not.

---
+++ LEAPYR.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+int yr;
+printf("ENTER YEAR");
+scanf("%d ",&yr);
+
+if(yr%4==0)
+{
+ printf("YEAR IS A LEAP YEAR");
+
+}
+  else
+  {
+
+  printf("YEAR IS NOT A LEAP YEAR");
+
+  }
+  getch();
+
+  }
