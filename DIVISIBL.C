9027fe60dabc53de9647d58cc041fb59880dc8fd
Bhumika Singh Rawat <bhumika282007@gmail.com>
2025-09-08 19:15:29 +0530

    DIVISIBLE BY 5 AND 11
    Check whether a number is divisible by 5 and 11.

---
+++ DIVISIBL.C
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void main()
+{
+int num;
+
+printf("ENTER ANY NUMBER:");
+scanf("%d",&num);
+
+if(num%5==0 && num%11==0)
+{
+printf("NUMBER IS DIVISIBLE BY 5 AND 11");
+}
+else
+{
+printf("NUMBER IS NOT DIVISIBLE BY 5 AND 11");
+}
+
+getch();
+
+}
