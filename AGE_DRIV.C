5a7dfa01d3029b07d02f3fd457e1500a7039aa5a
Bhumika Singh Rawat <bhumika282007@gmail.com>
2025-09-15 20:10:08 +0530

    ELIGIBLE TO DRIVE
    Check whether a person is eligible to drive (based on age).

---
+++ AGE_DRIV.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main()
+{
+
+int age;
+
+printf("ENTER YOU AGE:");
+scanf("%d",&age);
+
+if(age>=20)
+{
+printf("ELIGIBLE TO DRIVE :)");
+}
+else
+{
+printf("NOT ELIGIBLE TO DRIVE :(");
+}
+
+getch();
+
+}
